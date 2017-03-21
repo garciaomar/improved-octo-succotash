454522a03e15ea10317a5afe7220e4fa5c7e9414
Omar <omarg.1393@gmail.com>
2017-03-21 17:44:25 -0600

    Changed tesseract output file from txt to hocr

--- src/ocr.py
+++ src/ocr.py
@@ -3,22 +3,29 @@ import os
 def extract_text(filename, img):
     name = filename[:-4]
     outputfile = name + "_out"
+    options = "-l spa hocr"
     os.chdir("/textfiles")
-    os.system("tesseract " + filepath + outputfile)
+    os.system("tesseract " + filepath + outputfile + options)
     return outputfilename
 
-def parse_txt(filepath):
-    txt = open(filepath, 'r')
+def parse(filepath):
+    try:
+        hocr = open(filepath, 'r')
+    except:
+        print("File not found")
+        quit()
+    xml_tags = ['div', 'p', 'html', 'title', 'meta', 'head', 'body', 'doctype', '?xml']
+    ignore_tags = list()
     table = dict()
+    for tag in xml_tags:
+        ignore_tags.append(tag + '>')
+        ignore_tags.append('<' + tag)
 
-    #Remove header of the file
-    i = 1
-    while(i < 9):
-        txt.readLines()
-        i++
-    #Tag every state
-    for line in txt.readLines():
-        if line != ""
-            table = {line, []}
-        if line == "TOTAL"
-            break #stop tagging
+    for line in hocr.readlines():
+        ignored = False
+        for tag in ignore_tags:
+            if tag in line:
+                ignored = True
+                break
+        if ignored:
+            continue
