b793c0923c50f17642f14f28553fb3c61189ea5f
Omar <omarg.1393@gmail.com>
2017-04-02 16:12:25 -0500

    Continuation of the parse algorithm

--- src/ocr.py
+++ src/ocr.py
@@ -17,6 +17,8 @@ def parse(filepath):
     xml_tags = ['div', 'p', 'html', 'title', 'meta', 'head', 'body', 'doctype', '?xml']
     ignore_tags = list()
     table = dict()
+    top = None
+    bottom = None
     for tag in xml_tags:
         ignore_tags.append(tag + '>')
         ignore_tags.append('<' + tag)
@@ -29,3 +31,30 @@ def parse(filepath):
                 break
         if ignored:
             continue
+        if 'span' in line:
+            elements = line.split('span')
+            words = list()
+            for element in elements:
+                if 'ocrx_word' in element:
+                    content = element[element.index('">') + 2 : -2].strip()
+                    if 'bbox' in element:
+                        start = element.index('bbox')
+                        end = element[start : ].index('"')
+                        bbox = element[start + 5: start + end]
+                    word = wordbox(bbox, content)
+                    if content == 'Aguascalientes':
+                        top = word
+                    elif content == 'TOTAL':
+                        bottom = word
+                    last = None
+                    try:
+                        last = words[-1]
+                    except:
+                        pass
+                    if last is not None and word.same(last):
+                        last.merge(word)
+                    else:
+                        words.append(word)
+            if len(words) > 0:
+                lines.append(words)
+hocr.close()
